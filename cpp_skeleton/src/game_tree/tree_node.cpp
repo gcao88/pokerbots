fe01e3844b96d9fc4fc27851445f8dad13058f4d
Atharv Jain <atharvjain05@gmail.com>
2024-01-18 20:25:59 -0500

    first case for creating new gamestates

--- cpp_skeleton/src/game_tree/tree_node.cpp
+++ cpp_skeleton/src/game_tree/tree_node.cpp
@@ -7,14 +7,49 @@ using namespace std;
 struct Node {
     // leads to all direct children in the gametree 
     vector<pair<string, Node*>> children; 
-    // stores the flop for the current gametree we are solving
     // next action 
-    string action;
+    string action;  
 
-    Node(vector<string> cards, string _action="P1C") { // start building the game tree from here 
+    /*
+        key for what the actions mean: 
+        P12 means P1 is going to get 2 cards at this stage, P13 3 cards
+        similarly P22 P23
+    */
+    // WARNING: you should put cards in that dont have the cards on the flop 
+    Node(vector<int> cards, string _action = "P12") { // start building the game tree from here 
         action = _action;
-        if (action == "P1C") { // this action means we need to give player 1 a card
-
+        if (action == "P12" || action == "P22") {
+            for (int i = 0; i < cards.size(); i++) {
+                for (int j = i + 1; j < cards.size(); j++) {
+                    auto c1 = cards[i];
+                    auto c2 = cards[j]; 
+                    auto new_cards = cards; 
+                    new_cards.erase(find(new_cards.begin(), new_cards.end(), c1)); 
+                    new_cards.erase(find(new_cards.begin(), new_cards.end(), c2)); 
+                    if (action == "P12")
+                        children.push_back(make_pair(to_string(c1) + " " + to_string(c2), new Node(new_cards, "P23"))); 
+                    else
+                        children.push_back(make_pair(to_string(c1) + " " + to_string(c2), new Node(new_cards, "F")));
+                }
+            }
+        } else if(action == "P13" || action == "P23") {
+            for (int i = 0; i < cards.size(); i++) {
+                for (int j = i + 1; j < cards.size(); j++) {
+                    for (int k = j + 1; k < cards.size(); k++) {
+                        auto c1 = cards[i];
+                        auto c2 = cards[j]; 
+                        auto c3 = cards[k];
+                        auto new_cards = cards; 
+                        new_cards.erase(find(new_cards.begin(), new_cards.end(), c1)); 
+                        new_cards.erase(find(new_cards.begin(), new_cards.end(), c2)); 
+                        new_cards.erase(find(new_cards.begin(), new_cards.end(), c3)); 
+                        if (action == "P13")
+                            children.push_back(make_pair(to_string(c1) + " " + to_string(c2) + " " + to_string(c3), new Node(new_cards, "P22"))); 
+                        else
+                            children.push_back(make_pair(to_string(c1) + " " + to_string(c2) + " " + to_string(c3), new Node(new_cards, "F")));
+                    }
+                }
+            }
         }
     } 
 
